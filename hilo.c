abe5c675f1bdab7251ff048c525dff5792228b94
Jonathan <jparrales15@gmail.com>
2017-08-06 22:35:35 -0500

    corregido

--- hilo.c
+++ hilo.c
@@ -5,13 +5,14 @@
 #include <pthread.h>
 #define MAX 1000000
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-typedef struct _control{
+char ** palabras;
+int * contadas;
+/*typedef struct _control{
     int init;
     int end;
     char *doc;
     char *buscadas;
-} control;
+} control;*/
 
 //nos devuelve el numero de lineas del archivo
 int numero_lineas(char *ruta, int *tam_lineas){
@@ -36,107 +37,103 @@ int numero_lineas(char *ruta, int *tam_lineas){
     return -1;
 }
 
-control *numeroHilo(char *ruta,int num_hilos,char* buscadas){
-    int *lineas;
-    int di,modulo,acum=0,num_lineas=numero_lineas(ruta,lineas);
-    control *arreglo=(control *)malloc (sizeof(control)*num_hilos);   
-    modulo=num_lineas%num_hilos;
-    if(modulo == 0){
-    //escogeremos el entero menor de la division
-        di=num_lineas/num_hilos;
-        
-        for(int i=0;i<num_hilos;i++)
-        {
-            arreglo[i].init=acum;
-            acum=acum+(di-1);
-            arreglo[i].end=acum;
-            acum=acum+1;
-            arreglo[i].doc=ruta;
-            arreglo[i].buscadas=buscadas;
-        }  
-    }else{
-        //cogemos el entero mayor
-        //cuando la division no es exacta
-        di=num_lineas/num_hilos;
-        int n=num_hilos-1;
-        for(int i=0;i<num_hilos;i++)
-       {
-            if(n>=acum)
-            {
-                arreglo[i].init=acum;
-                acum=acum+(di-1);
-                arreglo[i].end=acum;
-                acum=acum+1;
-                arreglo[i].doc=ruta;
-                arreglo[i].buscadas=buscadas;
-            }
-            //coge el resto de los caracteres
-            else{
-                arreglo[i].init=acum;
-                acum=num_lineas-1;
-                arreglo[i].end=acum;
-                arreglo[i].doc=ruta;
-                arreglo[i].buscadas=buscadas;
-            }
-            n=n-di;
-       }  
-    }
-    return arreglo;
-}
-
 void * buscador(void* ctrl)
 {
-    char line[MAX];
-    control *data = (control *)ctrl;
-    char *p;
-    char *buscar=data.buscadas;
-    FILE *fp=fopen(data.doc,"rt");;
-    int i=0;
-    char aux[MAX];
-    pthread_mutex_lock(&mutex);
-    fgets(line,MAX,fp);
-    while(!feof(fp)) {
-                   i++;
-                   if(p=strstr(line,"addres")) { /* tomo como subpalabra "addres" 
-                   si se encuentra, sscanf guarda en aux la palabra que contiene a la subpalabra "addres"*/
-                     sscanf(p,"%s",aux);
-                     }
-                     if(!strcmp(aux,"addres")) /* comparo las palabras, y en caso de ser iguales sale del while */
-                         break;
-                   fgets(line,MAX,fp);
-    }
-    pthread_mutex_unlock(&mutex);
-    fclose(fp);
-    
-    
-    return (void *)line;
+    char * str = (char *) ctrl;
+
+
+	pthread_mutex_lock(&mutex);
+
+		
+	char *alto;
+	alto = strtok(str," \"!·$%&/()=º|@#~½¬<>-_'ç`+*[]{}ḉç¿,.!?:;");
+    while( alto != NULL ){
+    	if(!strcmp(*(palabras), alto)){
+    		*(contadas) = *(contadas) + 1;
+    	}
+    	else if(!strcmp(*(palabras+1), alto)){
+    		*(contadas+1) = *(contadas+1) + 1;
+    	}
+    	else if(!strcmp(*(palabras+2), alto)){
+    		*(contadas+2) = *(contadas+2) + 1;
+    	}
+       	alto = strtok(NULL, " \"!·$%&/()=º|@#~½¬<>-_'ç`+*[]{}ḉç¿,.!?:;");
+   	}
+		
+
+   	printf("Resultados encontrados: \nPalabra: %s - > Cantidad: %d\nPalabra: %s - > Cantidad: %d\nPalabra: %s - > Cantidad: %d\n",*(palabras), *(contadas), *(palabras+1), *(contadas+1), *(palabras+2), *(contadas+2));
+
+	
+	pthread_mutex_unlock(&mutex);
+	return (void *)0;
 }
 
 int main(int argc, char *argv[]){
+    int *ncaracteres;
+    char ** bloques;
+    int cont=0;
 
-    pthread_t thr[atoi(argv[2])];
-    int i,rc;
-    char *palabras;
-    char *buscadas;
-    palabras= (char *) malloc(sizeof(char)*MAX);
+    palabras = malloc(sizeof(char*)*3);
+	contadas = malloc(sizeof(int)*3);
+    
     if (argc==1) {
-		printf("./Taller12 <ruta> <numero_hilos> <palabra1> <palabra2> <palabra3>");
+		printf("./Hilos <ruta> <numero_hilos> <palabra1> <palabra2> <palabra3>");
 		exit(1);
 	}
+
     for(int k=3;k<argc;k++)
     {
-        buscadas[k-3]=argv[k];
+        palabras[k-3]=argv[k];
+        contadas[k-3]=0;
     }
-    control *arreglo=numeroHilo(atoi(argv[1]),atoi(argv[2]),buscadas);
-    for (i = 0; i < atoi(argv[2]); i++) {
-        if ((rc = pthread_create(&thr[i], NULL, buscador, &arreglo[i]))) {
-            fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
-            return EXIT_FAILURE;
-        }
+
+    int nlineas = numero_lineas(argv[1], 0);
+    ncaracteres=malloc(sizeof(int)*nlineas);
+    numero_lineas(argv[1], ncaracteres);
+    for(int i = 1; i < nlineas ; i++){
+        cont  += *(ncaracteres+i);
     }
-    for (i = 0; i < atoi(argv[2]); ++i) {
-        pthread_join(thr[i], &palabras);
-        //printf("Valor Hilo %d: %lu\n",(i+1),(char)palabras);
-    }
+
+    int divisor = cont/atoi(argv[2]);
+
+	int seek = 0;
+    bloques = malloc(sizeof(char*)*atoi(argv[2]));
+
+    for(int i = 0 ; i < atoi(argv[2]) ; i++){
+        
+        FILE * file = fopen(argv[1],"r");
+        fseek(file, seek,SEEK_SET); 
+        *(bloques+i) = malloc(sizeof(char)*divisor);
+
+        for( int j = 0 ; j < divisor ; j++){
+            char c = getc(file);
+            *(*(bloques+i)+j) = c;    
+        }      
+        seek = seek + divisor;
+	}
+
+    int p;
+	pthread_t * threads = malloc(sizeof(pthread_t)*atoi(argv[2]));
+
+	for(int i = 0; i < atoi(argv[2]); i++){
+
+		p = pthread_create(threads+i, NULL, buscador, (void *)*(bloques+i));
+		if(p < 0){
+			fprintf(stderr, "Error al crear el hilo : %d\n", i);
+			exit(-1);	
+		}
+
+
+	}
+	for(int j = 0; j < atoi(argv[2]); j++){
+
+		p = pthread_join(threads[j], NULL);
+		if(p < 0){
+			fprintf(stderr, "Error al esperar por el hilo\n");
+			exit(-1);
+		}
+
+	}
+
     return 0;
 }
