1db61c6bdd9098427d86dfca975b6eb666b21700
Jonathan <jparrales15@gmail.com>
2017-08-06 20:03:11 -0500

    con fallas

--- hilo.c
+++ hilo.c
@@ -1,21 +1,20 @@
-#include <time.h>
 #include <stdio.h>             /* for convenience */
 #include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 #include <pthread.h>
-#include <math.h>
+#define MAX 1000000
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct _control{
     int init;
     int end;
-    FILE *doc;
+    char *doc;
+    char *buscadas;
 } control;
 
-int aleatorio(int a, int b){
-    return (rand()%(b-a+1))+a;
-}
 //nos devuelve el numero de lineas del archivo
-int numero_lineas(char *rutas, int *tam_lineas){
+int numero_lineas(char *ruta, int *tam_lineas){
     if(ruta != NULL){
         FILE *ar = fopen(ruta,"r");
         int lineas =0;
@@ -37,29 +36,30 @@ int numero_lineas(char *rutas, int *tam_lineas){
     return -1;
 }
 
-control *numeroHilo(int num_hilos){
+control *numeroHilo(char *ruta,int num_hilos,char* buscadas){
     int *lineas;
-    int di,modulo,acum=0,num_lineas=numero_lineas(argc[1],lineas);
-    control *arreglo=(control *)malloc (sizeof(control)*b);   
-    modulo=num_hilos%numero_lineas;
+    int di,modulo,acum=0,num_lineas=numero_lineas(ruta,lineas);
+    control *arreglo=(control *)malloc (sizeof(control)*num_hilos);   
+    modulo=num_lineas%num_hilos;
     if(modulo == 0){
     //escogeremos el entero menor de la division
-        di=num_hilos/num_lineas;
+        di=num_lineas/num_hilos;
         
-        for(int i=0;i<b;i++)
+        for(int i=0;i<num_hilos;i++)
         {
             arreglo[i].init=acum;
             acum=acum+(di-1);
             arreglo[i].end=acum;
             acum=acum+1;
-            arreglo[i].doc=fopen(argc[1],"r");
+            arreglo[i].doc=ruta;
+            arreglo[i].buscadas=buscadas;
         }  
     }else{
         //cogemos el entero mayor
         //cuando la division no es exacta
-        di=(num_hilos/num_lineas);
+        di=num_lineas/num_hilos;
         int n=num_hilos-1;
-        for(int i=0;i<b;i++)
+        for(int i=0;i<num_hilos;i++)
        {
             if(n>=acum)
             {
@@ -67,14 +67,16 @@ control *numeroHilo(int num_hilos){
                 acum=acum+(di-1);
                 arreglo[i].end=acum;
                 acum=acum+1;
-                arreglo[i].doc=fopen(argc[1],"r");
+                arreglo[i].doc=ruta;
+                arreglo[i].buscadas=buscadas;
             }
             //coge el resto de los caracteres
             else{
                 arreglo[i].init=acum;
-                acum=a-1;
+                acum=num_lineas-1;
                 arreglo[i].end=acum;
-                arreglo[i].doc=fopen(argc[1],"r");
+                arreglo[i].doc=ruta;
+                arreglo[i].buscadas=buscadas;
             }
             n=n-di;
        }  
@@ -82,53 +84,59 @@ control *numeroHilo(int num_hilos){
     return arreglo;
 }
 
-double obtenerTiempoActual(){
-    struct timespec tsp;
-
-    clock_gettime( CLOCK_REALTIME, &tsp);
-
-    double secs = (double)tsp.tv_sec;
-    double nano = (double)tsp.tv_nsec/1000000000.0;
-
-    return secs+nano;
+void * buscador(void* ctrl)
+{
+    char line[MAX];
+    control *data = (control *)ctrl;
+    char *p;
+    char *buscar=data.buscadas;
+    FILE *fp=fopen(data.doc,"rt");;
+    int i=0;
+    char aux[MAX];
+    pthread_mutex_lock(&mutex);
+    fgets(line,MAX,fp);
+    while(!feof(fp)) {
+                   i++;
+                   if(p=strstr(line,"addres")) { /* tomo como subpalabra "addres" 
+                   si se encuentra, sscanf guarda en aux la palabra que contiene a la subpalabra "addres"*/
+                     sscanf(p,"%s",aux);
+                     }
+                     if(!strcmp(aux,"addres")) /* comparo las palabras, y en caso de ser iguales sale del while */
+                         break;
+                   fgets(line,MAX,fp);
+    }
+    pthread_mutex_unlock(&mutex);
+    fclose(fp);
+    
+    
+    return (void *)line;
 }
-
-
 
 int main(int argc, char *argv[]){
 
     pthread_t thr[atoi(argv[2])];
     int i,rc;
-    long sumatotal;
-    long *arr;
-    arr= (long*) malloc(sizeof(long)*atoi(argv[1]));
+    char *palabras;
+    char *buscadas;
+    palabras= (char *) malloc(sizeof(char)*MAX);
     if (argc==1) {
 		printf("./Taller12 <ruta> <numero_hilos> <palabra1> <palabra2> <palabra3>");
 		exit(1);
 	}
-    
-    srand(time(0));
-    double ti = obtenerTiempoActual();
-    for(i=0;i<atoi(argv[1]);i++){
-        arr[i]=aleatorio(50,200);
+    for(int k=3;k<argc;k++)
+    {
+        buscadas[k-3]=argv[k];
     }
-    control *arreglo=numeroHilo(atoi(argv[1]),atoi(argv[2]));
+    control *arreglo=numeroHilo(atoi(argv[1]),atoi(argv[2]),buscadas);
     for (i = 0; i < atoi(argv[2]); i++) {
-        arreglo[i].arr=arr;
-        if ((rc = pthread_create(&thr[i], NULL, sumador, &arreglo[i]))) {
+        if ((rc = pthread_create(&thr[i], NULL, buscador, &arreglo[i]))) {
             fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
             return EXIT_FAILURE;
         }
     }
-    void *sumaparcial=NULL;
     for (i = 0; i < atoi(argv[2]); ++i) {
-        pthread_join(thr[i], &sumaparcial);
-        printf("Valor Hilo %d: %lu\n",(i+1),(long)sumaparcial);
-        sumatotal =sumatotal+ (long)sumaparcial;
+        pthread_join(thr[i], &palabras);
+        //printf("Valor Hilo %d: %lu\n",(i+1),(char)palabras);
     }
-    double tf = obtenerTiempoActual()-ti;
-    printf("Suma Total: %d\n",(int)sumatotal);
-
-	printf("Tiempo de ejecucion: %9f segundos\n",tf);
     return 0;
 }
